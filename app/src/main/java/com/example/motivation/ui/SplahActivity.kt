bafac60f6972afcbe78ee38f2f499151b7bd3cd7
AlexandreMoura1988 <alexandremoura@live.com.pt>
2021-03-03 18:08:36 +0000

    SharedPreferences

--- app/src/main/java/com/example/motivation/ui/SplahActivity.kt
+++ app/src/main/java/com/example/motivation/ui/SplahActivity.kt
@@ -1,18 +1,26 @@
-package com.example.motivation
+package com.example.motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splah.*
 
 
 // splashActivity é o primeiro screen que o utilizador ira ver, trocar no manifests
 class SplahActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splah)
+
+        mSecurityPreferences = SecurityPreferences(this)
 
         //retirar a actionBar
         if(supportActionBar != null) {
@@ -35,6 +43,8 @@ class SplahActivity : AppCompatActivity(), View.OnClickListener {
 
         val name = editName.text.toString()
         if(name != "") {
+
+            mSecurityPreferences.storeString("name", name)
             //Navegaçao entre activities
             startActivity(Intent(this, MainActivity::class.java))
 
