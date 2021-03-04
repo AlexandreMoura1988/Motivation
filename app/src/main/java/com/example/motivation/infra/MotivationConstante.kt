08f123aaff02a587e22c816647076b01e7c145d9
AlexandreMoura1988 <alexandremoura@live.com.pt>
2021-03-04 15:04:37 +0000

    Click Events - MainActivity

--- app/src/main/java/com/example/motivation/infra/MotivationConstante.kt
+++ app/src/main/java/com/example/motivation/infra/MotivationConstante.kt
@@ -6,4 +6,11 @@ class MotivationConstante private constructor() {
         val PERSON_NAME = "name"
     }
 
+    object PHRASEFILTER {
+
+        val ALL = 1
+        val HAPPY = 2
+        val MORNING = 3
+    }
+
 }
--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.motivation.R
 import com.example.motivation.infra.MotivationConstante
 import com.example.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
 
@@ -19,5 +20,53 @@ class MainActivity : AppCompatActivity() {
         mSecurityPreferences = SecurityPreferences(this)
         textName.text = mSecurityPreferences.getString(MotivationConstante.KEY.PERSON_NAME)
 
+        if(supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+        buttonNewPhrase.setOnClickListener(this)
+        imageAll.setOnClickListener(this)
+        imageHappy.setOnClickListener(this)
+        imageMorning.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        val id = v?.id
+        val listFilter = listOf(R.id.imageAll, R.id.imageHappy, R.id.imageMorning)
+
+        if (id == R.id.buttonNewPhrase) {
+            handleNewPhrase()
+
+        } else if (id in listFilter) {
+            if (id != null) {
+                handleFilter(id)
+            }
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        imageAll.setColorFilter(resources.getColor(R.color.white))
+        imageHappy.setColorFilter(resources.getColor(R.color.white))
+        imageMorning.setColorFilter(resources.getColor(R.color.white))
+
+        when (id) {
+            R.id.imageAll -> {
+                imageAll.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.imageHappy -> {
+                imageHappy.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.imageMorning -> {
+                imageMorning.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+
+        }
+    }
+
+    private fun handleNewPhrase() {
+
     }
 }
+
